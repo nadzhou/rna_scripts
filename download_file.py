8a229651f92d58144852a7a9350cc3ff181758df
navid <navid@biome.io>
2022-02-13 20:25:36 +0500

    intitial

--- download_file.py
+++ download_file.py
@@ -1,10 +1,13 @@
 import logging
 import requests
-
+import subprocess
 
 
 def main():
-
+    file = "links.txt"
+    links = get_links(file)
+    for link in links:
+        download_data(link)
 
 def get_links(files):
     with open(file, "r") as file:
@@ -13,7 +16,10 @@ def get_links(files):
     return data.split()
 
 def download_data(url):
-
+    command = url.split()
+    command.insert(0, "wget")
+    r = subprocess.Popen(command, shell=True)
+    r.communicate()
 
 
 if __name__ == "__main__":
