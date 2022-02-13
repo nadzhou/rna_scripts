d01efe2978aefeef5d1428e5cca469e00b8208be
abbasi-zeeshan <zeeshanabbasi5800@gmail.com>
2022-02-13 21:37:54 +0500

    fixed execute file

--- execute_fastqc.py
+++ execute_fastqc.py
@@ -4,12 +4,14 @@ from pathlib import Path
 
 def main():
     dir = Path('sequences')
-    for file in dir.rglob(*.fastqc.gz):
+    for file in dir.rglob('*.fastq.gz'):
+        print(file)
         execute_fastqc(file)
 
 def execute_fastqc(file):
     command = f"fastqc {file}"
     command = command.split()
+    print(command)
     r = subprocess.Popen(command)
     r.communicate()
 
