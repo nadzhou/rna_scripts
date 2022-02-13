d1475af05079d0ba419a16ac8806f81659326ae1
navid <navid@biome.io>
2022-02-13 20:17:24 +0500

    intitial

---
+++ download_file.py
@@ -0,0 +1,20 @@
+import logging
+import requests
+
+
+
+def main():
+
+
+def get_links(files):
+    with open(file, "r") as file:
+        data = file.read()
+
+    return data.split()
+
+def download_data(url):
+
+
+
+if __name__ == "__main__":
+    main()
