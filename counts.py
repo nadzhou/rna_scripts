fc39358c3887b17cc9fcca79700cf11077d561e4
navid <navid@biome.io>
2022-03-13 22:02:18 +0500

    Refactor counts.py

--- counts.py
+++ counts.py
@@ -5,13 +5,15 @@ dir = Path('./scripts/')
 
 def main():
     df_list = []
-    for file in dir.rglob('*.counts'):
+    for file in dir.rglob('*counts*'):
         df = get_df(file)
         df_list.append(df)
 
-    merged = pd.concat(df_list)
+
+    merged = pd.concat(df_list, axis=1)
     print(merged)
 
+    merged.to_csv("designMatrix.txt", sep='\t')
 
 def get_df(count_df):
     columns = ['gene_id', f'{count_df.stem}']
